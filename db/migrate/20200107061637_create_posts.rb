3edeaa11e7bc1942a97b2fe11fb17306ae4178b8
uesin <kazukihana87@gmail.com>
2020-01-07 16:10:55 +0900

    カラムの追加

--- db/migrate/20200107061637_create_posts.rb
+++ db/migrate/20200107061637_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+    t.string :title,null: false
+    t.text :text,limit:10000
+    
 
       t.timestamps
     end
--- db/migrate/20200107061648_create_users.rb
+++ db/migrate/20200107061648_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+    t.string :nicname,null:false,unique:true
+    t.string :email,null:false,unique:true
+    t.string :password,null:false
+    t.text :icon
+    t.text :text,limit:10000
 
       t.timestamps
     end
